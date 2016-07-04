196d897f57391595f76cfac21ea64272f53961e9
hilaryledoan <hilaryledoan126@gmail.com>
2016-07-04 01:11:20 -0700

    finished 1st draft of portfolio with 3 works

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@
 
     //owl carousel
     $('.owl-carousel').owlCarousel({
-      autoPlay: 5000, //Set AutoPlay to 3 seconds
+      autoPlay: 10000, //Set AutoPlay to 10 seconds
  
       items : 1,
       itemsDesktop : [1199,1],
